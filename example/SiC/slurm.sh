f002de6e727f696df967c52422ee8b364ad108de
WuXing <wuxingxing20g@ict.ac.cn>
2023-09-01 16:31:29 +0800

    change example files

---
+++ example/SiC/slurm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --partition=3090
+#SBATCH --job-name=mlff
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-task=1
+
+PWMLFF train linear_sic.json
