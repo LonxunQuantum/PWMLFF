c5bdc43145957ff0b3395ad167c65f07334d31b4
WuXing <stars_sparkling@163.com>
2025-01-13 16:09:27 +0800

    fix bug of plot virial when the ere is not dft virial

--- src/aux/inference_plot.py
+++ src/aux/inference_plot.py
@@ -7,9 +7,16 @@ def inference_plot(data_dir:str):
     num_atom = np.loadtxt(os.path.join(data_dir, "image_atom_nums.txt"))
     dft_E   = np.loadtxt(os.path.join(data_dir, "dft_total_energy.txt")) / num_atom
     MLFF_E  = np.loadtxt(os.path.join(data_dir, "inference_total_energy.txt")) / num_atom
+    rmse_E = np.sqrt(np.square(dft_E - MLFF_E).mean())
+    E_min = min(dft_E.min(), MLFF_E.min())
+    E_max = max(dft_E.max(), MLFF_E.max())
+
     dft_F   = np.loadtxt(os.path.join(data_dir, "dft_force.txt")).flatten()
     MLFF_F  = np.loadtxt(os.path.join(data_dir, "inference_force.txt")).flatten()
-    # 补充virial dft的图像
+    rmse_F = np.sqrt(np.square(dft_F - MLFF_F).mean())
+    F_min = min(dft_F.min(), MLFF_F.min())
+    F_max = max(dft_F.max(), MLFF_F.max())
+
     dft_V  = np.loadtxt(os.path.join(data_dir, "dft_virial.txt"))
     MLFF_V = np.loadtxt(os.path.join(data_dir, "inference_virial.txt"))
     filtered_indices = dft_V.flatten() > -1e6
@@ -18,17 +25,10 @@ def inference_plot(data_dir:str):
 
     dft_V = dft_V.flatten()[filtered_indices]
     MLFF_V = MLFF_V.flatten()[filtered_indices]
-
-    rmse_E = np.sqrt(np.square(dft_E - MLFF_E).mean())
-    rmse_F = np.sqrt(np.square(dft_F - MLFF_F).mean())
-    rmse_V = np.sqrt(np.square(dft_V - MLFF_V).mean())
-
-    E_min = min(dft_E.min(), MLFF_E.min())
-    E_max = max(dft_E.max(), MLFF_E.max())
-    F_min = min(dft_F.min(), MLFF_F.min())
-    F_max = max(dft_F.max(), MLFF_F.max())
-    V_min = min(dft_V.min(), MLFF_V.min())
-    V_max = max(dft_V.max(), MLFF_V.max())
+    if len(dft_V) > 0:
+        rmse_V = np.sqrt(np.square(dft_V - MLFF_V).mean())
+        V_min = min(dft_V.min(), MLFF_V.min())
+        V_max = max(dft_V.max(), MLFF_V.max())
         
     plt.plot(dft_E, MLFF_E,"o",markersize=3,c="C0")
     plt.plot([E_min,E_max],[E_min,E_max],"--",lw=1.2,c="C1")
@@ -60,20 +60,21 @@ def inference_plot(data_dir:str):
     plt.savefig(os.path.join(data_dir, "Force.png"), dpi=360)
     plt.close()
 
-    plt.plot(dft_V, MLFF_V,"o",markersize=3,c="C0")
-    plt.plot([V_min,V_max],[V_min,V_max],"--",lw=1.2,c="C1")
-    plt.axis([V_min+1.02,V_max*1.02,V_min*1.02,V_max*1.02])
-    s = r"RMSE of Virial is %.3f eV/atom" % rmse_V
-    ax = plt.gca()
-    plt.text(.15,.05,s,fontsize=size,transform=ax.transAxes)
-    plt.xticks(size=size-4)
-    plt.yticks(size=size-4)
-    plt.xlabel(r"DFT Virial (eV)",size=size)
-    plt.ylabel(r"MLFF Virial (eV)",size=size)
-    #plt.title(title,size=size+4)
-    plt.tight_layout()
-    plt.savefig(os.path.join(data_dir, "Virial.png"), dpi=360)
-    plt.close()
+    if len(dft_V) > 0:
+        plt.plot(dft_V, MLFF_V,"o",markersize=3,c="C0")
+        plt.plot([V_min,V_max],[V_min,V_max],"--",lw=1.2,c="C1")
+        plt.axis([V_min+1.02,V_max*1.02,V_min*1.02,V_max*1.02])
+        s = r"RMSE of Virial is %.3f eV/atom" % rmse_V
+        ax = plt.gca()
+        plt.text(.15,.05,s,fontsize=size,transform=ax.transAxes)
+        plt.xticks(size=size-4)
+        plt.yticks(size=size-4)
+        plt.xlabel(r"DFT Virial (eV)",size=size)
+        plt.ylabel(r"MLFF Virial (eV)",size=size)
+        #plt.title(title,size=size+4)
+        plt.tight_layout()
+        plt.savefig(os.path.join(data_dir, "Virial.png"), dpi=360)
+        plt.close()
 
 if __name__=="__main__":
-    inference_plot("/data/home/wuxingxing/datas/pwmat_mlff_workdir/auag/debug/dp/test_result")
+    inference_plot("/data/home/wuxingxing/datas/pwmat_mlff_workdir/hfo2/nep_all_pwmlff/lkf_1b/test/test_result")
